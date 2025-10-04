bc13f3faecf79fced3b41a667d941c2cff1ce5e0
Jord4nKing <jord4nking1986@gmail.com>
2025-10-04 09:20:38 +0100

    Downgraded next and eslint to more stable version

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,3 @@
-// @ts-expect-error – Next.js mis-types dynamic route params in this version
 export default function Page({ params }: { params: { slug: string } }) {
     return <main>Slug is: {params.slug}</main>
   }
